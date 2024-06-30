22eefaa4c7f8bfef374ba1bc97af23a762040398
Mateusz Rusyn <mateusz.rusyn@gmail.com>
2024-06-30 23:56:16 +0200

    Add enums for language & tmdb data

---
+++ app/Enums/LanguageType.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Enums;
+
+enum LanguageType: string
+{
+    case EN = 'en';
+    case PL = 'pl';
+    case DE = 'de';
+}
---
+++ app/Enums/TmdbDataType.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Enums;
+
+enum TmdbDataType: string
+{
+    case MOVIE = 'movie';
+    case SERIE = 'tv';
+}
