b4f17fabefbeb98e840f69e7e59f2e5d7884c2ba
Mateusz Rusyn <mateusz.rusyn@gmail.com>
2024-06-30 23:56:36 +0200

    Add routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\MovieController;
+use App\Http\Controllers\MovieGenreController;
+use App\Http\Controllers\SerieController;
+use App\Http\Controllers\SerieGenreController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('{lang}/movies', [MovieController::class, 'index']);
+Route::get('{lang}/series', [SerieController::class, 'index']);
+Route::get('{lang}/movie/genres', [MovieGenreController::class, 'index']);
+Route::get('{lang}/serie/genres', [SerieGenreController::class, 'index']);
