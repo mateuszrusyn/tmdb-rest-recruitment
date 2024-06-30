362c543477a159044f876d1519d31f0b8b794075
Mateusz Rusyn <mateusz.rusyn@gmail.com>
2024-06-30 23:54:28 +0200

    Add migrations for:
    - Movie model
    - Serie model
    - MovieGenre model
    - SerieGenre model

---
+++ database/migrations/2024_06_30_135701_create_movies_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('tmdb_id')->unique();
+            $table->string('release_date');
+            $table->float('vote_average');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('movies');
+    }
+};
